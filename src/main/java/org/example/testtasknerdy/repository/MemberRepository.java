b46e5a7c7099e6b4b5f105bb5b41cb8f4108d036
iivvakii <ivan.stefanyuk.2004.m@gmail.com>
2024-08-13 12:47:51 +0300

    Revert "Create all necessary class"
    This reverts commit eb88ccf9e7c63c431ba8e44e698ff7d38f36e2dc.

--- src/main/java/org/example/testtasknerdy/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.testtasknerdy.repository;
-
-import org.example.testtasknerdy.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
